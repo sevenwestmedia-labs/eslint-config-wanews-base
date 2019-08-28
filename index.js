a08014c5e97212ba5ba43c9dc13e738fb694c0d0
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-08-28 22:13:25 +0800

    fix: Change array-type to simple
    Array<> is easier to read for generic types, but shorthand is nicer to use in most scenarios

--- index.js
+++ index.js
@@ -15,5 +15,8 @@ module.exports = {
         "@typescript-eslint/no-non-null-assertion": 0,
         "@typescript-eslint/no-use-before-define": 0,
         "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/array-type": {
+            default: "simple",
+        },
     },
 }
