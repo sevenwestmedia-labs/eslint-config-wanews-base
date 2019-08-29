f5f88d68531514d4c9b689ce64bc427cce30c2ae
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-08-29 09:40:44 +0800

    chore: Amend trim whitespace option

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ module.exports = {
         "@typescript-eslint/no-non-null-assertion": 0,
         "@typescript-eslint/no-use-before-define": 0,
         "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/array-type": {
-            default: "simple",
-        },
+        "@typescript-eslint/array-type": [1, { default: "array-simple" }],
     },
 }
